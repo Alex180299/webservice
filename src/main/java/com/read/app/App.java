0f3316c4b79a353a98c99f8b04078075a7822c87
Alejandro Ortiz <alejandroortiz@Mac-mini-de-Alejandro.local>
2020-06-09 10:18:26 -0500

    2.2.1.0 - Corrigiendo errores de log

--- src/main/java/com/read/app/App.java
+++ src/main/java/com/read/app/App.java
@@ -8,16 +8,13 @@ import com.read.app.service.ReadLayouts;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.config.ConfigurationSource;
-import org.apache.logging.log4j.core.config.xml.XmlConfiguration;
 import org.quartz.ScheduleBuilder;
-
-import java.io.FileInputStream;
 
 import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
 
 public class App
 {
+
     private static final Logger log = LogManager.getLogger(App.class.getSimpleName());
 
     private static ReadLayouts readLayouts = new ReadLayouts();
@@ -25,7 +22,6 @@ public class App
     public static void main(String[] args)
     {
         ConfigurationModule.configurationFile = "./config/configuration.xml";
-        //ConfigurationModule.loggingFile = "./config/log4j.xml";
         ConfigurationModule.layoutFieldsFile = "./config/layout.xml";
         ConfigurationModule.layoutFiltersFile = "./config/filters.xml";
         init();
@@ -36,6 +32,18 @@ public class App
         Configuration configuration = null;
         try
         {
+            try
+            {
+                LoggerContext context = LoggerContext.getContext(true);
+                context.reconfigure();
+            }
+            catch (Exception e)
+            {
+                log.fatal("No se puede configurar el archivo logger debiado a : " + e.getMessage() + " la aplicacion se cerrara ");
+                System.exit(-1);
+            }
+            log.info("Se ha cargado el logger correctamente");
+
             configuration = readLayouts.readConfig();
             ReadLayoutSchedule readLayoutSchedule = new ReadLayoutSchedule();
             ScheduleBuilder scheduleBuilder = null;
--- src/main/java/com/read/app/configuration/ConfigurationModule.java
+++ src/main/java/com/read/app/configuration/ConfigurationModule.java
@@ -2,9 +2,7 @@ package com.read.app.configuration;
 
 public class ConfigurationModule
 {
-    public static String directoryConfig;
     public static String configurationFile;
-    //public static String loggingFile;
     public static String layoutFieldsFile;
     public static String layoutFiltersFile;
 }
